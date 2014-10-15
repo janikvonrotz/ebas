515f15ee8df288e8b2b745c465b2c7594fbad8e4
Ubuntu <ubuntu@ip-172-30-1-233.eu-west-1.compute.internal>
2014-10-15 17:22:40 +0000

    init

---
+++ ebas.js
@@ -0,0 +1,30 @@
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014 Twitter, Inc.
+ * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+ * details, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+// See the Getting Started docs for more information:
+// http://getbootstrap.com/getting-started/#support-ie10-width
+
+(function () {
+  'use strict';
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.querySelector('head').appendChild(msViewportStyle)
+  }
+})();
+
+
+
+var options = {
+  valueNames: [ 'Id', 'Beschreibung', 'Ort', 'Datum', 'Typ' ]
+};
+
+var userList = new List('ebas', options);
