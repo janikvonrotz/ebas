cbfc9d4955e36f4102816726931e04cddf04d23a
Janik von Rotz <login@janikvonrotz.ch>
2014-11-10 17:48:48 +0100

    :poop: :factory:
    :punch: :lantern:

--- function.php
+++ function.php
@@ -65,7 +65,7 @@ function DBConnect(){
 $Config = getConfig(); ?>
 
   <nav class="navbar navbar-default" role="navigation">
-    <div class="container-fluid">
+    <div class="container">
 
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
